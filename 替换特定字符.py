e5161b1dcb94dd812dad8b9b1f52b0f9c94acdf4
lwchghy <lwchghy@gmail.com>
2018-04-05 20:21:38 +0800

    1

---
+++ 替换特定字符.py
@@ -0,0 +1,11 @@
+import sys
+import math
+import os
+path="f:\\BaiduNetdiskDownload\\中国科学技术大学-数学分析220讲\\文件夹1\\"
+f=os.listdir("f:\BaiduNetdiskDownload\中国科学技术大学-数学分析220讲\文件夹1")
+n=0
+for i in f:
+    oldname=f[n]
+    print(oldname.strip("sjh"))
+    os.rename(path+oldname,path+oldname.strip("sjh"))
+    n+=1
